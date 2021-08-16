8ad2e65a77878077ef5b47775bb360466cc17fd7
Akkapelli Harshith <b19066@students.iitmandi.ac.in>
2021-08-16 14:49:48 +0530

    final version

---
+++ READFUNCTION.md
@@ -0,0 +1,12 @@
+We have implemented factorial.cpp
+
+Input:
+YOu should give a positive integer n as input
+
+Outputs:
+n!
+
+Implementation:
+It is just a dynamic programming approach
+
+dp[i] = dp[i-1]*i for all i;
--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,19 @@
 #include "functions.h"
+const int MX = 100;
+vector<int> v(100);
+v[1] = 1;
 
 int factorial(int n){
   if(n < 0){
     cout << "PLease enter positive number: ";
     cin >> n;
   }
+  if(v[n]){
+    return v[n];
+  }
     if(n!=1){
-	return(n * factorial(n-1));
+      v[n] = n * factorial(n - 1);
+      return v[n];
     }
     else return 1;
 }

c82b9d4e485a3372973e7f9633517962de54d064
jaswanthsaimanepalli <manepallijaswanthsai@gmail.com>
2021-08-29 17:08:49 +0530

    Done

--- functions.h
+++ functions.h
@@ -1,3 +1,4 @@
 void print_hello();
 int factorial(int n);
 int gcd(int a,int b);
+int add_rec_function(int a,int b);
--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main(){
     cout << "The factorial of 5 is " << factorial(5);
     int x=56,y=98;
     cout<<"GCD of two integers"<<X<<" and "<<y<<"is"<<gcd(x,y);
+    cout<<"GCD of two integers by recursive function"<<X<<" and "<<y<<"is"<<add_rec_function(x,y);
     return 0;
 }

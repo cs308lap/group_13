e5f370178daa7c3d511b8b9ac572f2c1e02cd736
jaswanthsaimanepalli <manepallijaswanthsai@gmail.com>
2021-08-29 17:03:56 +0530

    Done

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main(){
     print_hello();
     cout << "\n";
     cout << "The factorial of 5 is " << factorial(5);
+    int x=56,y=98;
+    cout<<"GCD of two integers"<<X<<" and "<<y<<"is"<<gcd(x,y);
     return 0;
 }

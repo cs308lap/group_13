6421905eba66bb10e79e095093dacc0bc8f55e3b
jaswanthsaimanepalli <manepallijaswanthsai@gmail.com>
2021-08-29 16:45:23 +0530

    Done

--- gcd.cpp
+++ gcd.cpp
@@ -1,5 +1,13 @@
 #include "functions.h"
-int gcd(int a,int b){
+int gcd(int x,int y){
+    if(x==0){return y;}
+    else if(y==0){return x;}
+    while(x!=y){
+        if(x>y){x=x-y;}
+        else{y=y-x;}
+    }
+}
+int add_rec_function(int a,int b){
     if (a == 0)
        return b;
     if (b == 0)
